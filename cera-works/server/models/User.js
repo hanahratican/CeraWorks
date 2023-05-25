d92d7e7f6e45631958ffb46b0ffeec828a04c2c5
Hanah R <hanahratican@gmail.com>
2023-05-24 20:06:32 -0700

    Added schemas to Review.js and User.js

--- cera-works/server/models/User.js
+++ cera-works/server/models/User.js
@@ -14,3 +14,28 @@ const userSchema = new Schema({
         required: [true, 'Please enter a password'],
     },
 });
+
+
+
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+}
+);
+
+userSchema.statics.login = async function (email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
+
+const User = model('user', userSchema);
+
+module.exports = User;
