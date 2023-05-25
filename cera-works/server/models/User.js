3d5b635d8601ef6bc0fd4ab0d25434096021b447
Hanah R <hanahratican@gmail.com>
2023-05-24 19:43:04 -0700

    Working on the models files

---
+++ cera-works/server/models/User.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter an email'],
+        unique: true,
+        lowercase: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password'],
+    },
