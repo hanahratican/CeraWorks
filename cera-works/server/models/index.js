d285fcaf29dddb8f32702180df5d7dc086436022
Hanah R <hanahratican@gmail.com>
2023-05-25 20:18:54 -0700

    Updated the index.js file in the models folder

--- cera-works/server/models/index.js
+++ cera-works/server/models/index.js
@@ -1,3 +1,6 @@
 const User = require('./User');
+// for some reason vs code wants to change this to const Schedule = require('./schedule'); with a lowercase s
+const Schedule = require('./schedule');
+const Review = require('./Review');
 
-module.exports = { User };
+module.exports = { User, Schedule, Review };
