dfc9d40aa12c471f0a9efecaf68ffa0491a56ddc
Ruben Franco <ruben.afranco@yahoo.com>
2023-06-01 20:57:57 -0700

    finished up the services page

--- cera-works/client/src/components/Services.jsx
+++ cera-works/client/src/components/Services.jsx
@@ -11,31 +11,50 @@ const Services = () => {
                     actual prices may vary based on the size and condition of the vehicle, and specific 
                     service requirements. We recommend contacting us directly for a personalized quote. </p>
             </div>
-                {/* Icon Container */}
-            <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' alt='service1' />
-                    <p></p>
+                {/*  Container */}
+            <div className='w-full grid grid-cols-2 gap-4 py-8'>
+                <div className='bg-neutral-200 text-zinc-800 shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
+                    <div className='font-bold text-xl mb-2 text-center py-3'>Basic Package <span className='text-[#F38442]'>$1,000</span></div>
+                    <ul className='max-w-md space-y-1 list-disc list-inside py-1'>                        
+                        <li>Single layer of ceramic coating for added protection and shine.</li>
+                        <li>Hand wash and prep of the vehicle's exterior.</li>
+                        <li>Application of ceramic coating to painted surfaces.</li>
+                        <li>Basic surface decontamination.</li>
+                        <li>Duration: Up to 2 years of protection.</li>
+                    </ul>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' alt='service2' />
-                    <p></p>
+                <div className='bg-neutral-200 text-zinc-800 shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
+                    <div className='font-bold text-xl mb-2 text-center py-3'>Standard Package <span className='text-[#F38442]'>$1,500</span></div>
+                    <ul className='max-w-md space-y-1 list-disc list-inside py-1'>
+                        <li>Two layers of ceramic coating for enhanced durability and gloss.</li>
+                        <li>Hand wash and prep of the vehicle's exterior.</li>
+                        <li>Application of ceramic coating to painted surfaces and trim.</li>
+                        <li>Paint decontamination and light paint correction (removing minor swirl marks 
+                            and imperfections)
+                        </li>
+                        <li>Duration: Up to 3.5 years of protection.</li>
+                    </ul>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' alt='service3' />
-                    <p></p>
+                <div className='bg-neutral-200 text-zinc-800 shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
+                    <div className='font-bold text-xl mb-2 text-center py-3'>Premium Package <span className='text-[#F38442]'>$2.000</span></div>
+                    <ul className='max-w-md space-y-1 list-disc list-inside py-1'>                        
+                        <li>Two layers of ceramic coating for maximum protection and gloss.</li>
+                        <li>Hand wash, clay bar treatment, and prep of the vehicle's exterior.</li>
+                        <li>Application of ceramic coating to painted surfaces, wheels, and trim. </li>
+                        <li>Extensive paint correction to remove swirl marks, scratches, and imperfections.</li>
+                        <li>Glass coating for improved visibility and water repellency. </li>
+                        <li>Duration: Up to 5 years of protection.</li></ul>
                 </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' alt='service4' />
-                    <p></p>
-                </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' alt='service5' />
-                    <p></p>
-                </div>
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto' alt='service6' />
-                    <p></p>
+                <div className='bg-neutral-200 text-zinc-800 shadow-md shadow-[#040c16] hover:scale-110 duration-500 rounded'>
+                    <div className='font-bold text-xl mb-2 text-center py-3'>Additional Services</div>
+                    <ul className='max-w-md space-y-1 list-disc list-inside py-1'>                        
+                        <li>Interior Ceramic application on Leather. <span className='text-[#F38442] font-bold'>$350</span></li>
+                        <li>Wheel Coating: <span className='text-[#F38442] font-bold'>$100</span> per wheel.</li>
+                        <li>Headlight Restoration: <span className='text-[#F38442] font-bold'>$75</span> </li>
+                        <li>Paint Correction (Two stage polish): <span className='text-[#F38442] font-bold'>$485</span> Small Car</li>
+                        <li>Paint Correction (Two stage polish): <span className='text-[#F38442] font-bold'>$685</span> Large Car / Trucks / SUV's </li>
+                        <li>Glass Coating: <span className='text-[#F38442] font-bold'>$50</span> per glass. <span className='text-[#F38442] font-bold'>$85</span> windshield.</li>
+                    </ul>
                 </div>
             </div>
         </div>
