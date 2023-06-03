f6bccb948f2af501367f2fe6ec4aa5e8c0cdff23
Hanah R <hanahratican@gmail.com>
2023-06-02 19:32:44 -0700

    Removed anything to do with schedule.

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,19 +1,15 @@
 const db = require('../config/connection');
 const { User, Review } = require('../models');
-// above also contained Schedule but it is not needed
 const userSeeds = require('./userSeed.json');
 const reviewSeeds = require('./reviewSeed.json');
-// const scheduleSeeds = require('./scheduleSeeds.json');
 
 db.once('open', async () => {
     try {
         await User.deleteMany({});
         await Review.deleteMany({});
-        // await Schedule.deleteMany({});
-    
+        
         await User.create(userSeeds);
         await Review.create(reviewSeeds);
-        // await Schedule.create(scheduleSeeds);
     
         console.log('all done!');
         process.exit(0);
