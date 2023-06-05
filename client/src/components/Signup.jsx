26c1c130a665e114328bdea52dcb9d8de7ef97f7
Ruben Franco <ruben.afranco@yahoo.com>
2023-06-04 22:31:18 -0700

    styled sign up page

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
 
 import { useMutation } from '@apollo/client';
 import { CREATE_USER } from '../utils/mutations';
@@ -38,28 +40,30 @@ const Signup = () => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
+    <main className="flex justify-center items-center h-screen bg-neutral-200">
+      <div className="w-96 p-6 shadow-lg bg-zinc-900 rounded-md">
         <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Sign Up</h4>
-          <div className="card-body">
+        <h4 className="text-3xl block text-center font-semibold text-neutral-200"><FontAwesomeIcon icon={faUser} style={{color: "#f38442", width:"50px"}}/>Sign Up</h4>
+          <div className="card-body mt-3">
             {data ? (
               <p>
                 Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
             ) : (
-              <form onSubmit={handleFormSubmit}>
+              <form className='mt-3' onSubmit={handleFormSubmit}>
+                <label className='"block mb-2 text-neutral-200 text-xl'>Email</label>
                 <input
-                  className="form-input"
+                  className="mt-3 form-input border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
                   placeholder="Your email"
                   name="email"
                   type="email"
                   value={formState.email}
                   onChange={handleChange}
                 />
+                <label className=' mt-3 block mb-2 text-neutral-200 text-xl'>Password</label>
                 <input
-                  className="form-input"
+                  className="mt-3 form-input border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600"
                   placeholder="******"
                   name="password"
                   type="password"
@@ -67,7 +71,7 @@ const Signup = () => {
                   onChange={handleChange}
                 />
                 <button
-                  className="btn btn-block btn-primary"
+                  className="btn btn-block btn-primary mt-5 border-2 border-[#f38442] bg-[#f38442] text-neutral-200 py-1 w-full rounded-md hover:bg-transparent hover:text-[#f38442] font-semibold"
                   style={{ cursor: 'pointer' }}
                   type="submit"
                 >
